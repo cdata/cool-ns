d59e7faace75254011cb5f98ac4c1f695fa31f63
Chris Joel <chris@scriptolo.gy>
2022-01-19 09:09:53 -0800

    Fix local deploy

--- scripts/local_deploy.sh
+++ scripts/local_deploy.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-TX=$( \
+CODE_ID=$( \
 junod tx wasm store \
   ./artifacts/cool_ns.wasm \
-  --from test-user \
-  --chain-id testing \
-  --gas auto \
+  --from test-user-local \
+  --gas 100000000 \
+  -b block \
   --output json \
-  -y | jq -r '.txhash'
+  -y | jq -r .logs[0].events[-1].attributes[0].value \
 )
 
-CODE_ID=$(junod query tx $TX --output json | jq -r '.logs[0].events[-1].attributes[0].value')
+echo "Smart contract code ID is $CODE_ID"
 
-echo $CODE_ID
+CONTRACT_ADDRESS=$( \
+junod tx wasm instantiate $CODE_ID \
+  '{"allowed_tlds": ["cool","rad"]}' \
+  --label coolns \
+  --from test-user-local \
+  -b block \
+  --output json \
+  -y | jq -r  .logs[0].events[0].attributes[0].value \
+)
+
+echo "Instance address is $CONTRACT_ADDRESS"
